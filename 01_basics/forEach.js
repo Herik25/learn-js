f796ee87539763339441749f09410d79e778e5eb
Herik25 <128887958+Herik25@users.noreply.github.com>
2024-04-26 10:29:51 +0530

    loops commit

---
+++ 01_basics/forEach.js
@@ -0,0 +1,15 @@
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8]
+
+// const a = myNums.filter(val => {
+//     return val > 4
+// })
+// const newNum = []
+// const b = myNums.forEach(val => {
+//     if (val > 4) {
+//         newNum.push(val)
+//     }
+// })
+
+// console.log(b);
+
+// console.log(a);
---
+++ 01_basics/forOf.js
@@ -0,0 +1,23 @@
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// const cars = ["BMW", "Volvo", "Mini"];
+
+// let text = "";
+// for (let x of cars) {
+//   text += x;
+// }
+// console.log(text);
+
+// const str = "hello harsh"
+
+// for (const s of str) {
+//     if (s === " ") {
+//         continue
+//     }
+//     console.log(s);
+// }
+
+
+// for (const key in arr) {
+//     console.log(key);
+// }
