04ed2d7372ac572b8800d0613fd07dbd1e5b4f8e
Herik25 <128887958+Herik25@users.noreply.github.com>
2024-05-09 12:59:33 +0530

    try catch 9-05

---
+++ Error/Script.js
@@ -0,0 +1,15 @@
+// const msg = "this is a global message"
+
+// try {
+//     const msg = "this is a try msg"
+//     throw "this is an error"
+//     console.log(msg);
+// } catch (e) {
+//     const msg = "this is a catch msg " + e
+//     console.log(msg);
+// } finally {
+//     msg = "this is finally msg"
+//     console.log("this is finally " + msg);
+// }
+
+// console.log("line 14 : ", msg);
--- Events/script.js
+++ Events/script.js
@@ -1,52 +1,55 @@
-window.addEventListener("load", function () {
-  alert("widnow is reloaded");
-});
+// window.addEventListener("load", function () {
+//   alert("widnow is reloaded");
+// });
 
-window.addEventListener("resize", function () {
-  alert("widnow is reloaded");
-});
+// window.addEventListener("resize", function () {
+//   alert("widnow is reloaded");
+// });
 
-window.addEventListener("scroll", function () {
-  alert("widnow is reloaded");
-});
+// window.addEventListener("scroll", function () {
+//   alert("widnow is reloaded");
+// });
 
-const btn = document
-  .getElementById("btn")
-  .addEventListener("click", helloHarsh);
-function helloHarsh() {
-  console.log("hello harsh");
-}
+// const btn = document
+//   .getElementById("btn")
+//   .addEventListener("click", helloHarsh);
+// function helloHarsh() {
+//   console.log("hello harsh");
+// }
 
-document.getElementById("btn").addEventListener("mouseover", () => {
-    alert("hello")
-})
+// document.getElementById("btn").addEventListener("mouseover", () => {
+//     alert("hello")
+// })
 
-document.addEventListener("keydown", () => {
-    console.log("key is pressed");
-})
+// document.addEventListener("keydown", () => {
+//     console.log("key is pressed");
+// })
 
-const form = document.getElementById("myForm").addEventListener("submit", (event) => {
-    event.preventDefault()
-    // console.log(event);
-    console.log("form is submited");
-})
+// const form = document.getElementById("myForm").addEventListener("submit", (event) => {
+//     event.preventDefault()
+//     // console.log(event);
+//     console.log("form is submited");
+// })
 
-window.addEventListener("click", () => {
-    console.log("window");
-},true)
-document.addEventListener("click", () => {
-    console.log("document");
-},true)
-document.getElementById("div2").addEventListener("click", (e) => {
+// window.addEventListener("click", () => {
+//     console.log("window");
+// },true)
+// document.addEventListener("click", () => {
+//     console.log("document");
+// },true)
+// document.getElementById("div2").addEventListener("click", (e) => {
 
-    console.log("div 2");
-},true)
-document.getElementById("div1").addEventListener("click", () => {
-    console.log("div 1");
-},true)
-document.getElementById("div3").addEventListener("click", () => {
-    console.log("div 3");
-},true)
-document.getElementById("btn").addEventListener("click", (e) => {
-    console.log("button");
-},true)
+//     console.log("div 2");
+// },true)
+// document.getElementById("div1").addEventListener("click", () => {
+//     console.log("div 1");
+// },true)
+// document.getElementById("div3").addEventListener("click", () => {
+//     console.log("div 3");
+// },true)
+// document.getElementById("btn").addEventListener("click", (e) => {
+//     console.log("button");
+// },true)
+
+const el = document.querySelectorAll(".items")
+console.log(el);
